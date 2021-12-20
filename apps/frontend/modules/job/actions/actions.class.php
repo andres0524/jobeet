f7d7bc3b28f39172a4834c3688254e5e2d721c98
andres0524 <38667480+andres0524@users.noreply.github.com>
2021-12-20 15:32:54 -0500

    6

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,11 +12,7 @@ class jobActions extends sfActions
 {
     public function executeIndex(sfWebRequest $request)
     {
-        $q = Doctrine_Query::create()
-            ->from('JobeetJob j')
-            ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
-
-        $this->jobeet_jobs = $q->execute();
+        $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
     }
 
     public function executeShow(sfWebRequest $request)
