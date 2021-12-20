b7d03bd0d58bdad70c79de8f0090a895fb4ee1ef
andres0524 <38667480+andres0524@users.noreply.github.com>
2021-12-20 16:03:01 -0500

    7

--- apps/frontend/modules/category/actions/actions.class.php
+++ apps/frontend/modules/category/actions/actions.class.php
@@ -10,11 +10,19 @@
  */
 class categoryActions extends sfActions
 {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
+    public function executeShow(sfWebRequest $request)
+    {
+        $this->category = $this->getRoute()->getObject();
+
+        $this->pager = new sfDoctrinePager(
+            'JobeetJob',
+            sfConfig::get('app_max_jobs_on_category')
+        );
+        $this->pager->setQuery($this->category->getActiveJobsQuery());
+        $this->pager->setPage($request->getParameter('page', 1));
+        $this->pager->init();
+    }
+
   public function executeIndex(sfWebRequest $request)
   {
     $this->forward('default', 'module');
