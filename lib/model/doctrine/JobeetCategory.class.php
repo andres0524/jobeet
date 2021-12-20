45e58f943afd3007346ee23c7e00dbf707f4c6eb
andres0524 <38667480+andres0524@users.noreply.github.com>
2021-12-20 15:52:10 -0500

    7

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -21,5 +21,17 @@ class JobeetCategory extends BaseJobeetCategory
 
         return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
     }
+    public function getSlug()
+    {
+        return Jobeet::slugify($this->getName());
+    }
+    public function countActiveJobs()
+    {
+        $q = Doctrine_Query::create()
+            ->from('JobeetJob j')
+            ->where('j.category_id = ?', $this->getId());
+
+        return Doctrine_Core::getTable('JobeetJob')->countActiveJobs($q);
+    }
 
 }
--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -16,7 +16,22 @@ class JobeetJobTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('JobeetJob');
     }
+    public function retrieveActiveJob(Doctrine_Query $q)
+    {
+        return $this->addActiveJobsQuery($q)->fetchOne();
+    }
+
     public function getActiveJobs(Doctrine_Query $q = null)
+    {
+        return $this->addActiveJobsQuery($q)->execute();
+    }
+
+    public function countActiveJobs(Doctrine_Query $q = null)
+    {
+        return $this->addActiveJobsQuery($q)->count();
+    }
+
+    public function addActiveJobsQuery(Doctrine_Query $q = null)
     {
         if (is_null($q))
         {
@@ -24,9 +39,11 @@ class JobeetJobTable extends Doctrine_Table
                 ->from('JobeetJob j');
         }
 
-        $q->andWhere('j.expires_at > ?', date('Y-m-d H:i:s', time()))
-            ->addOrderBy('j.expires_at DESC');
+        $alias = $q->getRootAlias();
 
-        return $q->execute();
+        $q->andWhere($alias . '.expires_at > ?', date('Y-m-d H:i:s', time()))
+            ->addOrderBy($alias . '.created_at DESC');
+
+        return $q;
     }
 }
