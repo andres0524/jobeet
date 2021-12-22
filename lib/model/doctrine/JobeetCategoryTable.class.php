204c9e2c28e3273bf94a688f15d8479c069851ff
andres0524 <38667480+andres0524@users.noreply.github.com>
2021-12-22 15:44:44 -0500

    19

--- lib/model/doctrine/JobeetCategoryTable.class.php
+++ lib/model/doctrine/JobeetCategoryTable.class.php
@@ -25,12 +25,22 @@ class JobeetCategoryTable extends Doctrine_Table
         return $q->execute();
     }
 
-    public function findOneBySlug($slug)
+    public function doSelectForSlug($parameters)
+    {
+        return $this->findOneBySlugAndCulture($parameters['slug'], $parameters['sf_culture']);
+    }
+
+    public function findOneBySlugAndCulture($slug, $culture = 'en')
     {
         $q = $this->createQuery('a')
             ->leftJoin('a.Translation t')
-            ->andWhere('t.lang = ?', 'en')
+            ->andWhere('t.lang = ?', $culture)
             ->andWhere('t.slug = ?', $slug);
         return $q->fetchOne();
+    }
+
+    public function findOneBySlug($slug)
+    {
+        return $this->findOneBySlugAndCulture($slug, 'en');
     }
 }
